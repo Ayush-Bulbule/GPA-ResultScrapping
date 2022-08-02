e2e7968ed3e8876dc3419dca507af0fb1d46c100
Ayush Bulbule <ayushbulbule24@gmail.com>
2022-08-03 00:00:08 +0530

    Added result scrapping script

--- main.py
+++ main.py
@@ -7,3 +7,55 @@
 # pip install requests
 # pip install bs4
 # pip install html5lib
+""" 
+<form action="result.php" target="_blank" method="post">
+<input type="text" name="regno">
+<input type="text" name="ucap_text" id="CaptchaInput" class="form-control" required="">
+</form>
+ """
+
+import requests
+
+from bs4 import BeautifulSoup, Tag
+
+url = "http://gpamravati.ac.in/result"
+
+
+# Step 1: Get the HTML
+
+r = requests.get(url)
+
+htmlcontent = r.content
+# print(htmlcontent)
+
+# Step 2: Parse the HTML
+soup = BeautifulSoup(htmlcontent, 'html.parser')
+# print(soup.prettify)
+
+
+# Step 3: HTML tree Traversal
+title = soup.title
+print(title.string)
+
+
+# Commonly used type of objects:
+
+# 1. Tag
+# 2. Navigable String
+# 3. BeautifulString
+# 4. Comment
+
+
+# get all td of the page
+paras = soup.find_all('td')
+
+
+captcha_box = soup.find('td', {"id": "CaptchaDiv"})
+
+print(captcha_box)
+
+
+# Get Text From ELements:
+captcha_text = captcha_box.get_text()
+
+print(captcha_text)
---
+++ result.py
@@ -0,0 +1,1 @@
+# Python Automation Script to get Result of all student from college website.

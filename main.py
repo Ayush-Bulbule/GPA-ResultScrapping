121c14510e84ac852a0d60c071cc23c19224f255
Ayush Bulbule <ayushbulbule24@gmail.com>
2022-08-02 20:50:45 +0530

    Webscrapping requirements added

---
+++ main.py
@@ -0,0 +1,9 @@
+# Scraping a website:
+# 1. Use API
+# 2. HTML web Scrapping using some tool like bs4 (beautifulsoup)
+
+
+# Requirements
+# pip install requests
+# pip install bs4
+# pip install html5lib
